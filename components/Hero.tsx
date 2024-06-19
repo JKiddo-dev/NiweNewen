2512f319d42b9009a8e31707b9b4eef164bb084e
AngelPailalef <angelpailalef@gmail.com>
2024-06-18 21:05:11 -0400

    cambiar pene

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='border-[#cc4949] bg-green-600 mx-auto max-w-[1920px] px-6 lg:px-20 3xl:px-0 flex flex-col gap-20 py-10 pb-32 lg:py-20 xl:flex-row border-2'>
+    <section className='border-[#cc4949] bg-black mx-auto max-w-[1920px] px-6 lg:px-20 3xl:px-0 flex flex-col gap-20 py-10 pb-32 lg:py-20 xl:flex-row border-2'>
       <div className='absolute right-0 top-0 h-screen w-screen'/>
 
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
