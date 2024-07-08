0932107b7baf23e7167f09eebc909b85f429489f
JKiddo-dev <matias.aguilar63@gmail.com>
2024-07-08 18:57:05 -0400

    changed services and statistics description, changed metadata description

--- components/Services.tsx
+++ components/Services.tsx
@@ -23,9 +23,9 @@ export const ServiceCard = ({ title, description, imageSrc }: ServiceCardProps) 
 
 const services = [
   { id: 1, title: 'Estadía en Domo', description: 'Estancias para una inmersión completa en la naturaleza.', imageSrc: '/Interior.JPG' },
-  { id: 2, title: 'Yoga y Meditación', description: 'Diferentes técnicas para la relajación y el alivio del estrés.', imageSrc: '/Terapia.JPG' },
+  { id: 2, title: 'Meditación y Relajación', description: 'Diferentes técnicas para la relajación y el alivio del estrés.', imageSrc: '/Terapia.JPG' },
   { id: 3, title: 'Terapias Alternativas', description: 'Reiki online, flores de Bach, tapping y matrix reimprinting.', imageSrc: '/Flores de Bach.JPG' },
-  { id: 4, title: 'Experiencia en granja', description: 'Participa de la vida en el campo y la cosecha de productos frescos de temporada.', imageSrc: '/Cultivo.JPG' },
+  { id: 4, title: 'Experiencia en GRANJA', description: 'Participa de la vida en el campo y la cosecha de productos frescos de temporada.', imageSrc: '/Cultivo.JPG' },
   { id: 5, title: 'Clases de Pintura', description: 'Desata tu creatividad en un entorno natural e inspirador.', imageSrc: '/Pintura.JPG' },
   { id: 6, title: 'Bosque nativo', description: 'Adéntrate en un bosque nativo  y descubre la belleza de los pinos y la flora autóctona.', imageSrc: '/Bosque.JPG' },
 ];
